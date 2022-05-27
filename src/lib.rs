bbba0a739b12cb6d3760a727f72b5c544cb285f4
Guanqun Lu <guanqunlu@outlook.com>
2022-05-27 09:58:14 +0800

    fix: add a missing exported struct

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,4 @@ mod response_wrapper;
 pub use block_identifier::BlockIdentifier;
 pub use client::UEthers;
 pub use error::UEthersError;
-pub use models::{BlockRpcResponse, TransactionRpcResponse};
+pub use models::{BlockRpcResponse, FullBlockRpcResponse, TransactionRpcResponse};
